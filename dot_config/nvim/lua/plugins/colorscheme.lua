bad8801f65c196731d04134816aa46c02171c949
Travis Lyons <travis.lyons@sourcegraph.com>
2025-08-06 06:16:45 -0400

    Add .config/nvim/lua/plugins/colorscheme.lua Add .config/nvim/lua/plugins/markview.lua Add .config/nvim/lua/plugins/treesitter.lua

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  -- add gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = { contrast = "hard" },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "go",
+        "svelte",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
