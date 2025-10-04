d3b9858d879aca3d87af881f4d877c9aed0fa50e
Travis Lyons <travis.lyons@sourcegraph.com>
2025-10-04 01:02:29 -0400

    Update .config/nvim/lua/plugins/nvim-lspconfig.lua

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,10 +2,21 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
-      svelte = {},
+      svelte = {
+        capabilities = {
+          workspace = {
+            didChangeWatchedFiles = vim.fn.has("nvim-0.10") == 0 and { dynamicRegistration = true },
+          },
+        },
+      },
       marksman = {},
       ltex = {
         filetypes = { "markdown", "tex", "bib" },
+      },
+      ts_ls = {
+        init_options = {
+          maxTsServerMemory = 8192,
+        },
       },
     },
   },
