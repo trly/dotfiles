ab592ad6130a0adcd067e944e036fa38a2c51742
Travis Lyons <travis.lyons@sourcegraph.com>
2025-11-19 14:58:59 -0500

    Update .config/ghostty/config Update .config/mise/config.toml Update .config/nvim/lua/plugins/treesitter.lua Add .gitconfig-personal Update .gitconfig Update .zshrc

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,31 @@
 return {
-{
-"nvim-treesitter/nvim-treesitter",
-opts = function(_, opts)
-local add = {
-"bash",
-"css",
-"html",
-"go",
-"java",
-"javascript",
-"json",
-"lua",
-"markdown",
-"markdown_inline",
-"python",
-"query",
-"regex",
-"scss",
-"svelte",
-"tsx",
-"typescript",
-"vimdoc",
-"vim",
- "yaml",
- }
-  opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, add)
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      local add = {
+        "bash",
+        "css",
+        "html",
+        "go",
+        "java",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "rust",
+        "scss",
+        "svelte",
+        "tsx",
+        "typescript",
+        "vimdoc",
+        "vim",
+        "yaml",
+      }
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, add)
+    end,
+  },
 }
