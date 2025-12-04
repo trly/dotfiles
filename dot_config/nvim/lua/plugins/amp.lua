bcb71b71cf54b09e299452282f344d0b4c8b6d2c
Travis Lyons <travis.lyons@sourcegraph.com>
2025-12-04 15:49:30 -0700

    Update .config/nvim/lua/plugins/amp.lua Update .config/nvim/lua/plugins/mcphub.lua

--- dot_config/nvim/lua/plugins/amp.lua
+++ dot_config/nvim/lua/plugins/amp.lua
@@ -3,5 +3,6 @@ return {
   "sourcegraph/amp.nvim",
   branch = "main",
   lazy = false,
+
   opts = { auto_start = true, log_level = "info" },
 }
--- dot_config/nvim/lua/plugins/mcphub.lua
+++ dot_config/nvim/lua/plugins/mcphub.lua
@@ -2,7 +2,10 @@ return {
   "ravitemer/mcphub.nvim",
   lazy = false,
   priority = 10000,
+  build = "bundled_build.lua",
   config = function()
-    require("mcphub").setup()
+    require("mcphub").setup({
+      use_bundled_binary = true,
+    })
   end,
 }
