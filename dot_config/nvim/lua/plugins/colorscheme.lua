2e58a1ab33fd6a8f5b25803cc3a8f607e078db30
Travis Lyons <travis.lyons@sourcegraph.com>
2026-01-16 10:36:01 -0500

    Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,9 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      flavour = "auto", -- latte, frappe, macchiato, mocha
-      transparent_background = true, -- disables setting the background color.
-      background = { -- :h background
-        light = "latte",
-        dark = "frappe",
-      },
-    },
-  },
+	{
+		"f4z3r/gruvbox-material.nvim",
+		name = "gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
 }
