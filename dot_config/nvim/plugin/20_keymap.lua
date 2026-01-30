191d2fb105f42160e0c9b1285d17fe1a0f44c913
Travis Lyons <travis.lyons@sourcegraph.com>
2026-01-29 22:58:20 -0500

    Update .config/nvim/plugin/20_keymap.lua

--- dot_config/nvim/plugin/20_keymap.lua
+++ dot_config/nvim/plugin/20_keymap.lua
@@ -1,3 +1,24 @@
+-- This file contains definitions of custom general and Leader mappings.
+
+-- General mappings ===========================================================
+
+local nmap = function(lhs, rhs, desc)
+  -- See `:h vim.keymap.set()`
+  vim.keymap.set('n', lhs, rhs, { desc = desc })
+end
+
+-- Paste linewise before/after current line
+-- Usage: `yiw` to yank a word and `]p` to put it on the next line.
+nmap('[p', '<Cmd>exe "put! " . v:register<CR>', 'Paste Above')
+nmap(']p', '<Cmd>exe "put "  . v:register<CR>', 'Paste Below')
+
+-- Create a global table with information about Leader groups in certain modes.
+-- This is used to provide 'mini.clue' with extra clues.
+-- Add an entry if you create a new group.
+_G.Config.leader_group_clues = {
+  { mode = 'n', keys = '<Leader>f', desc = '+Find' },
+}
+
 -- Helpers for a more concise `<Leader>` mappings.
 -- Most of the mappings use `<Cmd>...<CR>` string as a right hand side (RHS) in
 -- an attempt to be more concise yet descriptive. See `:h <Cmd>`.
@@ -10,18 +31,9 @@ local xmap_leader = function(suffix, rhs, desc)
   vim.keymap.set('x', '<Leader>' .. suffix, rhs, { desc = desc })
 end
 
--- f is for 'Fuzzy Find'. Common usage:
--- - `<Leader>ff` - find files; for best performance requires `ripgrep`
--- - `<Leader>fg` - find inside files; requires `ripgrep`
--- - `<Leader>fh` - find help tag
--- - `<Leader>fr` - resume latest picker
--- - `<Leader>fv` - all visited paths; requires 'mini.visits'
---
--- All these use 'mini.pick'. See `:h MiniPick-overview` for an overview.
+-- f is for 'Find'.
 nmap_leader('ff', '<Cmd>Pick files<CR>',                        'Files')
 nmap_leader('fg', '<Cmd>Pick grep_live<CR>',                    'Grep live')
 nmap_leader('fG', '<Cmd>Pick grep pattern="<cword>"<CR>',       'Grep current word')
 nmap_leader('fh', '<Cmd>Pick help<CR>',                         'Help tags')
-
--- sr (search/replace) with GrugFar
-nmap_leader('sr', '<Cmd>GrugFar<CR>', 				'Search/Replace')
+nmap_leader('fr', '<Cmd>GrugFar<CR>', 				'Find/Replace')
