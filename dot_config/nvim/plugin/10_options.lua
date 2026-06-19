0c098dc241d0ece18303ac6daed052b22b7805c7
Travis Lyons <travis.lyons@sourcegraph.com>
2026-06-19 10:50:42 -0400

    nvim: enable wordwrap

--- dot_config/nvim/plugin/10_options.lua
+++ dot_config/nvim/plugin/10_options.lua
@@ -7,3 +7,7 @@ vim.g.maplocalleader = '\\' -- Use `\` as <localleader> key
 
 -- Sync yank/paste with system clipboard
 vim.opt.clipboard = 'unnamedplus'
+
+-- Soft-wrap long lines at the window edge, breaking at word boundaries
+vim.opt.wrap = true
+vim.opt.linebreak = true
