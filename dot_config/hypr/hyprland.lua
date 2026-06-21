836a619e9edee3cfd27d6450929097d356271f54
Travis Lyons <travis.lyons@gmail.com>
2026-06-20 23:31:01 -0400

    Update .config/hypr/hyprland.lua Update .config/noctalia/settings.json

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -315,6 +315,7 @@ hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = tr
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
+hl.bind("CTRL + SUPER + ALT + T", hl.dsp.exec_cmd(ipc .. " darkMode toggle"))
 
 
 --------------------------------
